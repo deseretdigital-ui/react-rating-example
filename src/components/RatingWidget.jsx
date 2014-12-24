e47ae68df4f1de5cf1e2b03c54f22575f52671ac
Russell Ahlstrom <rahlstrom@deseretdigital.com>
2014-12-24 10:17:25 -0700

    Adds Size to RatingWidget
    Adds the size prop to RatingWidget to control the number of RatingSteps.

--- src/components/RatingWidget.jsx
+++ src/components/RatingWidget.jsx
@@ -2,16 +2,36 @@ var React = require('react/addons');
 var RatingStep = require('./RatingStep');
 
 var RatingWidget = React.createClass({
+  propTypes: {
+    size: React.PropTypes.number
+  },
+
+  getDefaultProps: function() {
+    return {
+      size: 5
+    }
+  },
+
   render: function() {
+    var RatingSteps = this.renderRatingSteps();
+
     return (
       <div className="rating-widget">
-        <RatingStep type="whole" />
-        <RatingStep type="whole" />
-        <RatingStep type="whole" />
-        <RatingStep type="half" />
-        <RatingStep type="empty" />
+        {RatingSteps}
       </div>
     );
+  },
+
+  renderRatingSteps: function() {
+    var RatingSteps = [];
+
+    for (var i = 1; i <= this.props.size; ++i) {
+      RatingSteps.push(
+        <RatingStep type="empty" />
+      );
+    }
+
+    return RatingSteps;
   }
 });
 
