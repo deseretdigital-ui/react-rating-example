099fd7275bc7cc25154e804f2c165e5138fe7d53
Russell Ahlstrom <rahlstrom@deseretdigital.com>
2014-12-24 10:07:04 -0700

    Adds PropTypes and DefaultProps
    Updates RatingStep to only allow a specific values in the type
    attribute. All other values now output a console.log warning.
    
    Adds a default value for RatingStep.

--- src/components/RatingStep.jsx
+++ src/components/RatingStep.jsx
@@ -1,6 +1,16 @@
 var React = require('react/addons');
 
 var RatingStep = React.createClass({
+  propTypes: {
+    type: React.PropTypes.oneOf(['whole', 'half', 'empty'])
+  },
+
+  getDefaultProps: function() {
+    return {
+      type: 'empty'
+    };
+  },
+
   render: function() {
     var classes = {
       'rating-widget__step': true
